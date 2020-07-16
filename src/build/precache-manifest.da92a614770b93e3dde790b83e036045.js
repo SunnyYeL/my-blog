9a379893720e0bc43e44693917c025e2d333875e
SunnyYeL <distdistwd@gmail.com>
2020-07-16 16:03:20 +1000

    First commit

---
+++ src/build/precache-manifest.da92a614770b93e3dde790b83e036045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224549466032fdc541c5b056f678cc99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de82f4691102b7ed0b20",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "61b81a605b7c806e89e4",
+    "url": "/static/js/2.d4df99f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4df99f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de82f4691102b7ed0b20",
+    "url": "/static/js/main.a8d6b613.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
